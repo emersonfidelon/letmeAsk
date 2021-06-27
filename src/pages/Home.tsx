ba87f4aab838b0c750d7cc9ba001b945485bd245
Emerson Santana <emersonfidelon@gmail.com>
2021-06-26 23:37:28 -0300

    feat: create google firebase authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,27 @@
+import  { useHistory } from 'react-router-dom';
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
+import { auth, firebase } from '../services/firebase';
 
 import { Button } from '../components/Button'
 
 import '../styles/auth.scss'
 
 export function Home() {
+  const history = useHistory()
+
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(result => {
+      
+      history.push('/rooms/new')
+    })
+
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -20,7 +35,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="LetmeAsk" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
